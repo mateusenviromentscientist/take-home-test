c9f64670ef6f3a184ca839bb6058841a9dde9041
Mateus Santos <60739025+mateusenviromentscientist@users.noreply.github.com>
2025-12-14 17:10:16 -0300

    fix factory to run integrations via pipeline

--- backend/src/Fundo.Services.Tests/Integration/CustomWebApplicationFactory.cs
+++ backend/src/Fundo.Services.Tests/Integration/CustomWebApplicationFactory.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Fundo.Applications.Infra.Context;
 
 namespace Fundo.Services.Tests.Integration
 {
@@ -10,6 +12,8 @@ namespace Fundo.Services.Tests.Integration
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            builder.UseEnvironment("Test");
+
             builder.ConfigureServices(services =>
             {
                 services.AddAuthentication(options =>
@@ -19,6 +23,12 @@ namespace Fundo.Services.Tests.Integration
                 })
                 .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
                     TestAuthHandler.Scheme, _ => { });
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
             });
         }
     }
