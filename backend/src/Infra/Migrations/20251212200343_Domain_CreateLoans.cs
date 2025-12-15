a485e9daab30fa761a91baff0634702c07c1bc39
Mateus Santos <60739025+mateusenviromentscientist@users.noreply.github.com>
2025-12-15 07:32:12 -0300

    update gitignore e migrations

---
+++ backend/src/Infra/Migrations/20251212200343_Domain_CreateLoans.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fundo.Applications.Infra.Migrations
+{
+    public partial class Domain_CreateLoans : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Loans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrentBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ApplicantName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Loans", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Loans_Status",
+                table: "Loans",
+                column: "Status");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Loans");
+        }
+    }
+}
